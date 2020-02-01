b360c62c68da6ea17841dc802c43dc3c14484a3b
AB <abl0e@Aarons-MacBook-Pro.local>
2020-02-01 12:34:50 -0800

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friendMatch = [
+  {
+    name: "Brad Pitt",
+    photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2019%2F12%2Fbrad-2-1.jpg",
+    scores: ["2", "2", "2", "2", "2"]
+  }, 
+  {
+    name: "Kobe Bryant",
+    photo: "https://mk0slamonlinensgt39k.kinstacdn.com/wp-content/uploads/2018/02/kobe_lakers_free_agents.jpg",
+    scores: ["3", "3", "3", "3", "3"]
+  }, 
+  {
+    name: "Tom Brady",
+    photo: " https://images.daznservices.com/di/library/sporting_news/75/f8/tom-brady-010820-getty-ftrjpg_cv52i0ukyays1113tb12szkfv.jpg?t=-1985342625&quality=100",
+    scores: ["1", "1", "1", "1", "1"]
+  }, 
+  {
+    name: "Lamar Jackson",
+    photo: "https://cdn.cnn.com/cnnnext/dam/assets/191205005331-lamar-jackson-file-exlarge-169.jpg",
+    scores: ["3", "3", "3", "3", "3"]
+  },
+
+];
+
+
+
+module.exports = friendMatch;
